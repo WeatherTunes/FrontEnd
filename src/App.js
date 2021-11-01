8a464fc64b8bb776a037cd289c314ff1d94d4cc6
naffrey <nick.caffrey@icloud.com>
2021-11-01 14:42:31 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import React from 'react';
 import './App.css';
-// import AccountCreate from './components/AccountCreate/AccountCreate';
-import FirstMood from './components/MoodChecks/FirstMood'
+import FirstMood from './components/MoodChecks/FirstMood';
 import AngryPlaylist from './components/Playlist/Angry/AngryPlaylist';
 import EhhPlaylist from './components/Playlist/Ehh/EhhPlaylist';
 import HappyPlaylist from './components/Playlist/Happy/HappyPlaylist';
 import AccountCreate from './components/AccountCreate/AccountCreate';
 
 function App() {
-
-
-  return (
+	return (
 		<div>
-
 			<AccountCreate />
 			<FirstMood />
-			<HappyPlaylist/>
-      
-			<AngryPlaylist/>
-			<EhhPlaylist/>
+			<HappyPlaylist />
+
+			<AngryPlaylist />
+			<EhhPlaylist />
 		</div>
-  )
-		};
+	);
+}
 
 export default App;
