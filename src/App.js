61461967720e19129389c42730032f207a5e3c75
naffrey <nick.caffrey@icloud.com>
2021-11-01 13:25:53 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FirstMood from './components/MoodChecks/FirstMood'
 import AngryPlaylist from './components/Playlist/Angry/AngryPlaylist';
 import EhhPlaylist from './components/Playlist/Ehh/EhhPlaylist';
 import HappyPlaylist from './components/Playlist/Happy/HappyPlaylist';
-import { Route, Link } from 'react-router-dom'
 import AccountCreate from './components/AccountCreate/AccountCreate';
 
 function App() {
@@ -12,21 +11,7 @@ function App() {
 
   return (
 		<div>
-			{/* <div className="BLACK">
-    {/* <AccountCreate/> */}
-			{/* <Link className='Home' to='/'></Link>
-    <Link className='An gry' to='/angry'></Link>
-    <Link className='Ehh' to='Ehh'></Link>
-    <Link className='Happy' to='Happy'></Link> */}
-			{/* </div>
 
-    <div>
-      <Route exact path='/' component={FirstMood}/>
-      <Route exact path='/Angry' component={AngryPlaylist}/>
-      <Route exact path='/Ehh' component={EhhPlaylist}/>
-      <Route exact path='Happy' component={HappyPlaylist}/>
-    </div> */}{' '}
-			*/}
 			<AccountCreate />
 			<FirstMood />
 			<HappyPlaylist/>
@@ -34,6 +19,7 @@ function App() {
 			<AngryPlaylist/>
 			<EhhPlaylist/>
 		</div>
-	);}
+  )
+		};
 
 export default App;
