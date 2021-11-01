6af9c64f7602de9b8ac09b764afd572f0a2966fc
naffrey <nick.caffrey@icloud.com>
2021-11-01 15:37:19 -0500

    goooooo

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
-	<Router>
+	<React.StrictMode>
 		<App />
-	</Router>,
+	</React.StrictMode>,
 	document.getElementById('root')
 );
